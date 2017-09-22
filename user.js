b36a86061ba735c22c8b04ab64a4643819798ad9
rayzer <ray.leex@gmail.com>
2017-09-23 01:24:26 +0800

    Add auto jump from mebook page

--- user.js
+++ user.js
@@ -13,12 +13,12 @@
 (function () {
     'use strict';
     /*自定义CSS样式-开始*/
-    GM_addStyle('.my_baidu_link{color:green!important;text-decoration:none!important;border: 5px dotted green;padding: 2px;}');
+    GM_addStyle('.my_baidu_link{color:red!important;text-decoration:none!important;border: 1px dotted red;padding: 2px;}');
     /*自定义CSS样式-结束*/
-    
+
     var location = self.location;
     var location_pathname = location.pathname;
-    
+
     /**
     * 提取网盘链接中的密码
     * str  要匹配的字符串
@@ -35,15 +35,17 @@
 			return array.length;
 		}
     };
-    
+
     /**
     *渲染网盘链接
     */
     function activelink( baidu_pan_link_pattern ) {
         var baidu_pan_password;
+        var baidu_pan_link;
+
         $('.desc').each(function (index, value) {
             var password_pattern = /百度网盘密码[：:]?[ A-Za-z0-9]*/g;
-            
+
             var password_text = ($(this).html()).match(password_pattern);
             if (password_text) {
                 console.log("Found password text: "+ password_text);
@@ -53,27 +55,29 @@
                 }
                 baidu_pan_password = arrayTool.getUbound(passArray);
                 console.log("Found password text: " + baidu_pan_password);
-            } 
+            }
         });
-        
+
         $('.list').each(function (index, value) {
             var link = ($(this).html()).match(baidu_pan_link_pattern);
             if (link) {
                 var ss = $(this).html();
                 ss = ss.replace(baidu_pan_link_pattern, '$&" class="my_baidu_link"');
                 $(this).html(ss);
-                
+
                 $(value).find(".my_baidu_link").each(function (i, v) {
                     var o_href = $(v).attr("href");
-                    var n_href = o_href + "#" + baidu_pan_password
+                    var n_href = o_href + "#" + baidu_pan_password;
                     $(v).attr("href", n_href);
+                    baidu_pan_link = n_href;
                     //Debug
                     console.log("URL = " + o_href + " ---- " + "提取码 = " + baidu_pan_password);
                 });
-            } 
+            }
         });
+        return baidu_pan_link;
     }
-    
+
     /**
     *提取网盘链接中的密码
     */
@@ -84,19 +88,26 @@
         } else code = null;
         return code;
     };
-    
+
     $(function () {
-		//example: href="http://pan.baidu.com/s/1i53So53"
-        var baidu_pan_link_pattern = /href="http?:\/\/pan.baidu.com\/s\/\w*/g;
-        activelink(baidu_pan_link_pattern);
-        
+        //百度网盘链接自动跳转
+        if (window.location.href.indexOf("mebook.cc/download.php") != -1 ) {
+            //example: href="http://pan.baidu.com/s/1i53So53"
+            var baidu_pan_link_pattern = /href="http?:\/\/pan.baidu.com\/s\/\w*/g;
+            var new_address = activelink(baidu_pan_link_pattern);
+            window.location.href=new_address;
+        }
+
         //百度网盘提取码自动补全
         if (location_pathname.indexOf("/share/") != -1 && $("#accessCode") !== null) {
-            console.log("自动补全");
-            
             var code = getCode();
             $("#accessCode").val(code);
             $("#submitBtn").click();
         }
+
+        //TODO: 百度网盘自动进入目录
+
+        //TODO: 百度网盘自动下载azw3,mobi,epub之一
+        
     });
 })();
